c2c708e9ea65d15bb12e52152b42ea620d28b8da
Snigz <zolimagyarjr@gmail.com>
2015-12-26 23:27:46 +0100

    Inverse Matrix functions commit after test
    Functions are working, test run successful. 2015.12.26. 23:27

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,30 @@
-## Overall description
+## Caching the Inverse of a Matrix Functions
 
 
 ## This function creates a special "matrix" object that can cache its inverse
 ## set, get, set, get
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  m <- NULL
+  
+  set <- function(y) {
+    
+    x <<- y
+    m <<- NULL
+    
+    
+  }
+  get <- function() x
+  
+  set_inverse <- function(solve) m <<- inverse
+  get_inverse <- function() m
+  list(set = set, get = get,
+       set_inverse = set_inverse,
+       get_inverse = get_inverse                                                                                                                                                              = get_inverse
+       )
+  
+  
 }
 
 
@@ -15,4 +34,19 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  m <- x$get_inverse()
+  if(!is.null(m)) {
+    
+    message("getting cached data")
+    return(m)
+    
+    
+  }
+  data <- x$get()
+  m    <- solve(data, ...)
+  x$set_inverse(m)
+  m
+  
+  
 }
